5da82edc26f960dde7baca3fd2a39a9db606cdfe
Marsel Shannaq <marsel.shannaq@progressoft.com>
2019-10-19 22:15:17 +0300

    ffff

--- test.sh
+++ test.sh
@@ -3,5 +3,5 @@
 echo "this script is from github"
 NUMBER=$(($num1 + $num2))
 echo "$NUMBER"
-echo "marsel Shannaq11155552221"
+echo "marsel Shannaq1115555222ttttttt1"
 echo "this was run automagically"
